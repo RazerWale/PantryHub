5d054353207202a86fc9d8f26b36a4409310de78
Anton <anton333mai@gmail.com>
2023-08-30 21:30:20 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+
+try {
+    require_once('controllers/TestController.php');
+    require_once('controllers/UserController.php');
+
+    // controller here!
+    $test = new TestController();
+
+    $route = $_GET['action'] ?? null;
+
+    switch ($route) {
+        case 'test':
+            $test->test();
+            break;
+        default:
+            //should bring me to the home page
+            $test->default();
+            break;
+    }
+} catch (Throwable $e) {
+    // displayError($e->getMessage());
+    // echo $e->getMessage();
+    throw $e;
+    // die;
+    // displayTheError($e);
+}
---
+++ views/template.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>
+        <?= $title ?>
+        <!-- PHP -->
+    </title>
+    <link href="style.css" rel="stylesheet" />
+    <?= $style ?>
+    <!-- PHP -->
+</head>
+
+<body>
+    <?= $content ?>
+    <!-- PHP -->
+    <?= $script ?>
+    <!-- PHP -->
+</body>
+
+
+
+</html>
