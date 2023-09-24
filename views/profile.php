778e6f91c0b8f22dcb339720c393a25fa044246d
Tierra <tmthompson522@gmail.comclear>
2023-09-24 20:25:31 +0900

    changes to template and profile page

--- views/profile.php
+++ views/profile.php
@@ -1,76 +1,21 @@
 <?php $title = "Profile"; ?>
 
-<?php require_once('profileTemplate.php') ?>
+<?php /*require_once('profileTemplate.php')*/ ?>
 
 
 <?php ob_start() ?>
 
 <!--<link rel="stylesheet" href="path to the file">-->
+<link rel="stylesheet" href="views/css/profile.css">
 <style>
-    /* CSS for this page is here! */
-    body {
-        display: grid;
-        grid-template-columns: 300px 1fr;
-        grid-template-rows: 80px 1fr 1fr 1fr;
-    }
 
-    .logo {
-        grid-row-start: 1;
-        grid-row-end: 2;
-        grid-column-start: 1;
-
-    }
-
-    .profilemenu {
-        grid-row-start: 2;
-        grid-row-end: 5;
-        grid-column-start: 1;
-        grid-column-end: 2;
-        height: 90vh;
-        background-color: var(--tertiary-bg-color);
-    }
-
-    .profilemenu ul {
-        display: flex;
-        flex-direction: column;
-    }
-
-    .search-container {
-        grid-row-start: 1;
-        grid-row-end: 2;
-        grid-column-start: 2;
-        grid-column-end: 3;
-    }
-
-    .liked-recommend-buttons {
-        grid-row-start: 2;
-        grid-row-end: 3;
-        grid-column-start: 2;
-    }
-
-    .liked-recommend {
-        display: flex;
-        flex-direction: row;
-    }
-
-    .recommended-recipes,
-    .liked-recipe {
-        width: 90vw;
-    }
-
-    .recipe-links {
-        display: inline-block;
-        width: 30px;
-    }
-
-    .recipe-links img {
-        height: 20px;
-    }
 </style>
 
 <?php $style = ob_get_clean() ?>
-<main>
-    <div class="liked-recommend-buttons"><button>Recommended Recipes</button><button>Liked Recipes</button></div>
+
+<?php ob_start() ?>
+<main class="liked-recommended-container">
+    <div class="liked-recommend-buttons"><button class="recommend-button">Searched Recipes</button><button class="liked-button">Liked Recipes</button></div>
     <div class="liked-recommend">
         <div class="recommended-recipes">
             <?php
@@ -78,30 +23,34 @@
             foreach ($recipes as $recipe) { ?>
                 <div class="rec-recipe">
 
-                    <img src="https://spoonacular.com/recipeImages/<?= $recipe->getId() ?>-312x150.jpg" alt="">
-                    <div class="recipe-links">
-                        <img src="images/heart.svg" alt="">
-                        <img src="images/share.svg" alt="">
-                        <img src="images/timer.svg" alt="">
-                    </div>
-                    <a href="?action=recipePage&id=<?= $recipe->getId() ?>">
+                    <img src="https://spoonacular.com/recipeImages/<?= $recipe->getId() ?>-480x360.jpg" alt="">
+                    <!--<div class="recipe-link-container">
+                        <div class="recipe-links">
+                            <img src="images/heart.svg" alt="">
+                            <img src="images/share.svg" alt="">
+                            <img src="images/timer.svg" alt="">
+                        </div>
+                    </div>-->
+                    <a class="recipe-name" href="?action=recipePage&id=<?= $recipe->getId() ?>">
                         <?= $recipe->getName() ?>
                     </a>
-                    <div class="recipe-ingredients">
-                        <?php foreach ($recipe->getIngredients() as $ingredient) { ?>
-                            <div class="ingredients-needed">
-                                <?= $ingredient->getName() ?>
-                            </div>
+                    <ul class="recipe-ingredients">
+                        <li><a href="#">Ingredients</a>
+                            <ul class="dropdown">
+                                <?php foreach ($recipe->getIngredients() as $ingredient) { ?>
+                                    <li class="ingredient"><?= $ingredient->getName() ?></li>
 
-                        <?php } ?>
-                    </div>
+                                <?php } ?>
+                            </ul>
+                        </li>
+                    </ul>
                 </div>
 
             <?php } ?>
 
 
         </div>
-        <div class="liked-recipes">
+        <!--<div class="liked-recipes">
             <div class="liked-recipe">
                 <img src="https://spoonacular.com/recipeImages/636589-636x393.jpg" alt="">
                 <div class="recipe-links">
@@ -115,12 +64,12 @@
                     <div class="ingredients-owned">eggs, milk, chicken</div>
                 </div>
             </div>
-        </div>
+        </div>-->
     </div>
 </main>
 
 
-<?php ob_start() ?>
+<?php /*ob_start()*/ ?>
 
 
 <?php $content = ob_get_clean() ?>
